1bd1b661b051f212e5203dbe74612dc9699e477e
Michael Chan <michael.w.chan01@gmail.com>
2024-07-19 04:39:11 -0400

    Update todos routing and added todo methods

--- To-Do/data/todos.js
+++ To-Do/data/todos.js
@@ -4,4 +4,39 @@ let todos = [
     {id: 3, title: "Read a Book", description: 'Spend idle time reading', completed: true},
 ]
 
-module.exports = todos;
+function getTodos(){
+    return todos;
+}
+
+function getTodoById(id){
+    let todo = todos.find(todo => todo.id === id);
+    return todo;
+}
+
+function addTodo(todo){
+    todo.id = todos.length + 1;
+    todos.push(todo);
+    return todo;
+}
+
+function updateTodo(id, newTodo){
+    let todo = todos.find(todo => todo.id === id);
+    //Replace properties of object with newTodo
+    Object.assign(todo, newTodo);
+    return todo;
+}
+
+function deleteTodo(id){
+    let index = todos.findIndex(todo => todo.id === id);
+    todos.splice(index, 1);
+    return todos
+
+}
+
+module.exports = {
+    getTodos, 
+    getTodoById,
+    addTodo,
+    updateTodo,
+    deleteTodo
+}
